fa77cc3c5cd46ed69fe353c60031447c3eefa32c
Junichi Ito <jit@sonicgarden.jp>
2023-02-16 08:26:48 +0900

    Refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action -> { RequestStore.store[:current_ability] = current_ability }
+  before_action :store_current_ability
+
+  private
+
+  def store_current_ability
+    RequestStore.store[ApplicationRecord::CURRENT_ABILITY_KEY] = current_ability
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
+  CURRENT_ABILITY_KEY = :current_ability
+
   def self.with_force_authentication
     safer_initialize do |record|
-      if (ability = RequestStore.store[:current_ability])
+      if (ability = RequestStore.store[CURRENT_ABILITY_KEY])
         ability.authorize! :read, record
       else
         true
