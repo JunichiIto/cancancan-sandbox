81459caf191a991efbf62a51203429c2856e77a9
Junichi Ito <jit@sonicgarden.jp>
2023-02-16 08:22:05 +0900

    Move method to super class

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.with_force_authentication
+    safer_initialize do |record|
+      if RequestStore.store.key?(:current_user)
+        user = RequestStore.store[:current_user]
+        Ability.new(user).authorize! :read, record
+      else
+        true
+      end
+    end
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,3 @@
 class Project < ApplicationRecord
-  safer_initialize do |project|
-    if RequestStore.store.key?(:current_user)
-      user = RequestStore.store[:current_user]
-      Ability.new(user).authorize! :read, project
-    else
-      true
-    end
-  end
+  with_force_authentication
 end
