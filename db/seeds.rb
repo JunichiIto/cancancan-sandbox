40dcbdb9af78d1d078c15c12405059e99baae8f7
Junichi Ito <jit@sonicgarden.jp>
2023-02-15 16:36:57 +0900

    Add project pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.find_or_initialize_by(email: 'user@example.com', admin: false)
+user.password = user.password_confirmation = 'password'
+user.save!
+
+admin = User.find_or_initialize_by(email: 'adminr@example.com', admin: true)
+admin.password = admin.password_confirmation = 'password'
+admin.save!
+
+Project.find_or_create_by!(name: 'Active project', active: true)
+Project.find_or_create_by!(name: 'Inactive project', active: false)
