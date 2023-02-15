07107268c84ebb4aaf53d14ddb481b2592ebb0f7
Junichi Ito <jit@sonicgarden.jp>
2023-02-16 08:13:37 +0900

    Refactoring

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ApplicationRecord
-  safer_initialize message: self.to_s do |project|
+  safer_initialize do |project|
     if RequestStore.store.key?(:current_user)
       user = RequestStore.store[:current_user]
-      Ability.new(user).can? :read, project
+      Ability.new(user).authorize! :read, project
     else
       true
     end
--- config/initializers/safer_initialize.rb
+++
@@ -1,5 +0,0 @@
-SaferInitialize.configure do |config|
-  config.error_handle = -> (e) {
-    raise CanCan::AccessDenied.new("Not authorized!", :read, e.message.constantize)
-  }
-end
